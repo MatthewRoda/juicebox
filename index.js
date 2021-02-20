67dbe2f37599e0504f42176ec90c318757bca5ba
Matthew Roda <matthewroda12@gmail.com>
2021-02-20 13:07:33 -0700

    juicebox part2 ssetup and middleware

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
+//server init
 const PORT = 3000;
 const express = require('express');
 const server = express();
+const bodyParser = require('body-parser');
+server.use(bodyParser.json());
+
+const morgan = require('morgan');
+server.use(morgan('dev'));
+
+const { client } = require('./db');
+client.connect();
 
 server.listen(PORT, () => {
 	console.log('the server is up on port', PORT)
 });
+
+//routers
+const apiRouter = require('./api');
+server.use('/api', apiRouter);
 
 server.use((req, res, next) => {
 	console.log("<____Body Logger START____>");
@@ -13,3 +26,5 @@ server.use((req, res, next) => {
 	
 	next();
 });
+
+
